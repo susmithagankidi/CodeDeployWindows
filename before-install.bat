a7e70d55eaf5e9a903bea0ea25869d200d655626
susmithagankidi <gankidisusmitha@gmail.com>
2020-06-03 18:14:35 -0400

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,3 +1,3 @@
-echo "starting deployment"
-cd C:\inetpub\wwwroot\
-mkdir %APPLICATION_NAME%
+REM Install Internet Information Server (IIS). 
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
+c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
