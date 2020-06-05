ce445bf95b97af6b759ce22c920be369c2416931
susmithagankidi <gankidisusmitha@gmail.com>
2020-06-05 01:15:21 -0400

    Update prodScript.ps1

--- prodScript.ps1
+++ prodScript.ps1
@@ -7,7 +7,7 @@ $item
 Test-Path env:test_env
 if (Test-Path env:test_env) { 
     $item = "echo "
-    $item=Get-Content -Path Env:EnvironmentType
+    $item=Get-Content -Path Env:test_env
 
     if($item = "Production") {
         Copy-Item "C:\Sites\uat.xtracover.com\App_Data\Web.prod.config" -Destination "C:\Sites\uat.xtracover.com\Web.config" -Force
