65856b27e5f509e2d71b129ac4978403d2d78890
susmithagankidi <gankidisusmitha@gmail.com>
2020-06-05 20:04:53 -0400

    Update prodScript.ps1

--- prodScript.ps1
+++ prodScript.ps1
@@ -1,17 +1,17 @@
-$env:new=[System.Environment]::GetEnvironmentVariable("new")
+$env:new=[System.Environment]::GetEnvironmentVariable("test")
 $env:psmodulepath = $env:psmodulepath + ";"
 $item = "UAT_outside"
 $item
 $env:PATH
 $env:TMP
 gci env:
-$env:new
+$env:test
 $item
 whoami
-Test-Path env:new
-if (Test-Path env:new) { 
+Test-Path env:test
+if (Test-Path env:test) { 
     $item = "echo "
-    $item=Get-Content -Path Env:new
+    $item=Get-Content -Path Env:test
 
     if($item = "Production") {
         Copy-Item "C:\Sites\uat.xtracover.com\App_Data\Web.prod.config" -Destination "C:\Sites\uat.xtracover.com\Web.config" -Force
